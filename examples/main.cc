50a54d2f14e031c58c9f6370b5803cdf1f6dcb29
Gabriel Moreira <gabrielmscampos@gmail.com>
2021-09-22 11:29:35 -0300

    update eff map fo 2016postVFP and add eff map for 2017 & 2018

--- examples/main.cc
+++ examples/main.cc
@@ -6,15 +6,16 @@ int main() {
     // Inputs
     float jet_eta = 1.3;
     float jet_pt = 1500.0;;
+    // std::string filePath = "data/btageffmap-2016postVFP.json";
+    // std::string filePath = "data/btageffmap-2017.json";
+    std::string filePath = "data/btageffmap-2018.json";
 
     // BTagEffAnalyzer
     BTagEffAnalyzer effAnalyzer;
-    effAnalyzer.readFile(
-        "data/btageffmap-2016postVFP.json"
-    );
+    effAnalyzer.readFile(filePath);
 
     // Eval
-    double eff = effAnalyzer.getEfficiency("DY", jet_eta, jet_pt);
+    double eff = effAnalyzer.getEfficiency("TTTo2L2Nu", jet_eta, jet_pt);
     std::cout << "eff: " << eff << std::endl;
     
 }
